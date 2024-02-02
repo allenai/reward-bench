3e6b35c11e5d9feb49761ec49975f9369ac3263d
jacob-morrison <jacobm@allenai.org>
2024-02-02 13:29:38 -0800

    llama tokens

--- scripts/run_rm.py
+++ scripts/run_rm.py
@@ -175,7 +175,8 @@ def main():
         tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
         if 'gpt2' in args.model:
             model.config.pad_token_id = model.config.eos_token_id
-        if isinstance(tokenizer, LlamaTokenizer) or isinstance(tokenizer, LlamaTokenizerFast):
+        # if isinstance(tokenizer, LlamaTokenizer) or isinstance(tokenizer, LlamaTokenizerFast):
+        elif 'llama' in args.model or 'Llama' in args.model:
             num_added_tokens = tokenizer.add_special_tokens({
                 "bos_token": "<s>",
                 "eos_token": "</s>",
