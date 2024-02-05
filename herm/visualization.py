6abc62df753fc74df3ecc845b0c8690feb583903
ljvmiranda921 <ljvmiranda@gmail.com>
2024-02-05 13:55:50 -0800

    Make things cleaner

--- herm/visualization.py
+++ herm/visualization.py
@@ -16,6 +16,7 @@ def draw_model_source_histogram(
     figsize: Tuple[int, int] = (12, 8),
     normalize: bool = False,
     log_scale: bool = False,
+    top_n: Optional[int] = None,
 ) -> "matplotlib.axes.Axes":
     """Draw a histogram of the evaluation dataset that shows completion counts between models and humans.
 
@@ -25,6 +26,7 @@ def draw_model_source_histogram(
     figsize (Tuple[int, int]): control the figure size when plotting.
     normalize (bool): set to True to normalize the values based on total number completions.
     log_scale (bool): set the y-axis to logarithmic scale.
+    top_n (Optional[int]): if set, then only plot the top-n models in the histogram.
     RETURNS (matplotlib.axes.Axes): an Axes class containing the histogram.
     """
     dataset = datasets.load_dataset(dataset_name, split="filtered")
@@ -36,9 +38,6 @@ def draw_model_source_histogram(
     for example in dataset:
         for key in keys:
             model = example[key]
-            # There are duplicate instances of GPT4 and gpt-4 in the dataset
-            if model == "GPT4":
-                model = "gpt-4"
             models.append(model)
     counter = Counter(models)
 
@@ -51,6 +50,10 @@ def draw_model_source_histogram(
     fig, ax = plt.subplots(figsize=figsize)
     labels, values = zip(*counter.most_common())
 
+    if top_n:
+        labels = labels[:top_n]
+        values = values[:top_n]
+
     indices = np.arange(len(labels))
     width = 1
 
@@ -58,15 +61,23 @@ def draw_model_source_histogram(
     ax.set_xticks(indices, labels, rotation=90)
     ax.spines.right.set_visible(False)
     ax.spines.top.set_visible(False)
-    ax.set_title(f"Source of completions ({', '.join(keys)})")
+
+    title = f"Source of completions ({', '.join(keys)})"
 
     if normalize:
         ax.set_ylim(top=1.00)
+        title += " , normalized"
 
     if log_scale:
         ax.set_yscale("log")
+        title += ", log-scale"
 
+    if top_n:
+        title += f", showing top-{top_n}"
+
+    ax.set_title(title)
     fig.tight_layout()
+
     if output_path:
         print(f"Saving histogram to {output_path}")
         plt.savefig(output_path, transparent=True, dpi=120)
