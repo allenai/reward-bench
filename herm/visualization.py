f4f77b4d5a70e4c1d475df595adfe3291febc7db
ljvmiranda921 <ljvmiranda@gmail.com>
2024-02-05 13:36:33 -0800

    Move analysis script to visualization

--- herm/visualization.py
+++ herm/visualization.py
@@ -1,3 +1,5 @@
+"""Module for visualizing datasets and post-hoc analyses"""
+
 from collections import Counter
 from typing import List, Optional, Tuple
 
@@ -9,18 +11,20 @@ import numpy as np
 
 def draw_model_source_histogram(
     dataset_name: str = "ai2-adapt-dev/rm-benchmark-dev",
-    normalize: bool = False,
+    output_path: Optional[str] = None,
     keys: List[str] = ["chosen_model", "rejected_model"],
     figsize: Tuple[int, int] = (12, 8),
-    output_path: Optional[str] = None,
+    normalize: bool = False,
+    log_scale: bool = False,
 ) -> "matplotlib.axes.Axes":
     """Draw a histogram of the evaluation dataset that shows completion counts between models and humans.
 
     dataset_name (str): the HuggingFace dataset name to source the eval dataset.
-    normalize (bool): set to True to normalize the values based on total number completions.
+    output_path (Optional[Path]): if set, then save the figure in the specified path.
     keys (List[str]): the dataset columns to include in the histogram.
     figsize (Tuple[int, int]): control the figure size when plotting.
-    output_path (Optional[Path]): if set, then save the figure in the specified path.
+    normalize (bool): set to True to normalize the values based on total number completions.
+    log_scale (bool): set the y-axis to logarithmic scale.
     RETURNS (matplotlib.axes.Axes): an Axes class containing the histogram.
     """
     dataset = datasets.load_dataset(dataset_name, split="filtered")
@@ -31,7 +35,11 @@ def draw_model_source_histogram(
     models = []
     for example in dataset:
         for key in keys:
-            models.append(example[key])
+            model = example[key]
+            # There are duplicate instances of GPT4 and gpt-4 in the dataset
+            if model == "GPT4":
+                model = "gpt-4"
+            models.append(model)
     counter = Counter(models)
 
     if normalize:
@@ -47,12 +55,16 @@ def draw_model_source_histogram(
     width = 1
 
     ax.bar(indices, values, width)
-    ax.set_xticks(indices + width * 0.5, labels, rotation=90)
+    ax.set_xticks(indices, labels, rotation=90)
     ax.spines.right.set_visible(False)
     ax.spines.top.set_visible(False)
     ax.set_title(f"Source of completions ({', '.join(keys)})")
+
     if normalize:
         ax.set_ylim(top=1.00)
+
+    if log_scale:
+        ax.set_yscale("log")
 
     fig.tight_layout()
     if output_path:
