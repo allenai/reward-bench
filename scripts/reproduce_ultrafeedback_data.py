afa4d01dd2414c4fe06febf4bd51ff71ebcd00e4
jacob-morrison <jacobm@allenai.org>
2024-02-12 19:00:51 -0800

    fix

--- scripts/reproduce_ultrafeedback_data.py
+++ scripts/reproduce_ultrafeedback_data.py
@@ -3,4 +3,4 @@ from datasets import load_dataset
 ultrafeedback = load_dataset("openbmb/UltraFeedback")
 shp = load_dataset("stanfordnlp/SHP")
 anthropic_hh = load_dataset("Anthropic/hh-rlhf")
-openai_summarize = load_dataset("openai/summarize_from_feedback", split="comparisons")
+openai_summarize = load_dataset("openai/summarize_from_feedback", "comparisons")
