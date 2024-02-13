5352ce6019804092b4108d5887428d5c41149544
jacob-morrison <jacobm@allenai.org>
2024-02-12 18:57:48 -0800

    reproduction script

---
+++ scripts/reproduce_ultrafeedback_data.py
@@ -0,0 +1,6 @@
+from datasets import load_dataset
+
+ultrafeedback = load_dataset("openbmb/UltraFeedback")
+shp = load_dataset("stanfordnlp/SHP")
+anthropic_hh = load_dataset("Anthropic/hh-rlhf")
+openai_summarize = load_dataset("openai/summarize_from_feedback")
