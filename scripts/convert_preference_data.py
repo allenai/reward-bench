4df34d3e5b579e553b410ef1a243a952f7fe0e83
jacob-morrison <jacobm@allenai.org>
2024-02-13 20:43:04 -0800

    nectar full

--- scripts/convert_preference_data.py
+++ scripts/convert_preference_data.py
@@ -56,21 +56,23 @@ elif args.input_dataset == 'berkeley-nest/Nectar':
     for sample in dataset:
         prompt = sample['prompt'].replace("Human: ", "").replace("Assistant: ", "").strip()
         answers = sorted(sample['answers'], key=lambda x: x['rank'])
-        chosen = answers[0]['answer']
-        rejected = random_gen.choice(answers[1:])['answer']
-        chosen =  [
-            {'role': 'user', 'content': prompt},
-            {'role': 'assistant', 'content': chosen},
-        ]
-        rejected =  [
-            {'role': 'user', 'content': prompt},
-            {'role': 'assistant', 'content': rejected},
-        ]
-        new_data.append({
-            'chosen': chosen,
-            'rejected': rejected,
-            'source': 'nectar'
-        })
+        for i in range(len(answers) - 1):
+            for j in range(i + 1, len(answers)):
+                chosen = answers[i]['answer']
+                rejected = answers[j]['answer']
+                chosen =  [
+                    {'role': 'user', 'content': prompt},
+                    {'role': 'assistant', 'content': chosen},
+                ]
+                rejected =  [
+                    {'role': 'user', 'content': prompt},
+                    {'role': 'assistant', 'content': rejected},
+                ]
+                new_data.append({
+                    'chosen': chosen,
+                    'rejected': rejected,
+                    'source': 'nectar'
+                })
 elif args.input_dataset == 'argilla/ultrafeedback-binarized-preferences-cleaned':
     for sample in dataset:
         chosen = sample['chosen']
