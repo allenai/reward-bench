8dee1476ed30c7c05d10023274aad4372a46bb0b
Lj Miranda <ljvmiranda@gmail.com>
2024-02-13 19:49:04 -0800

    Update model loading

--- analysis/per_token_reward.py
+++ analysis/per_token_reward.py
@@ -17,6 +17,7 @@
 import argparse
 import logging
 import sys
+from typing import Any, Dict, List, Optional
 
 import torch
 import transformers
@@ -31,6 +32,47 @@ from transformers import (
     pipeline,
 )
 
+from herm import models
+
+REWARD_MODEL_CONFIG = {
+    "default": {
+        "model_builder": AutoModelForSequenceClassification.from_pretrained,
+        "pipeline_builder": pipeline,
+        "quantized": True,
+        "custom_dialogue": False,
+    },
+    "oasst": {
+        "model_builder": AutoModelForSequenceClassification.from_pretrained,
+        "pipeline_builder": pipeline,
+        "quantized": True,
+        "custom_dialogue": False,
+    },
+    "Starling": {
+        "model_builder": models.starling.build_starling_rm,
+        "pipeline_builder": models.starling.StarlingPipeline,
+        "quantized": False,
+        "custom_dialogue": False,
+    },
+    "openbmb": {
+        "model_builder": models.openbmb.LlamaRewardModel.from_pretrained,
+        "pipeline_builder": models.openbmb.OpenBMBPipeline,
+        "quantized": True,
+        "custom_dialogue": False,
+    },
+    "PairRM": {
+        "model_builder": models.pairrm.DebertaV2Model.from_pretrained,
+        "pipeline_builder": models.pairrm.PairRMPipeline,
+        "quantized": True,
+        "custom_dialogue": True,
+    },
+    "SHP": {
+        "model_builder": T5ForConditionalGeneration.from_pretrained,
+        "pipeline_builder": models.shp.SHPPipeline,
+        "quantized": True,
+        "custom_dialogue": True,
+    },
+}
+
 
 def get_args():
     """
@@ -41,87 +83,108 @@ def get_args():
         "--model",
         type=str,
         default="natolambert/gpt2-dummy-rm",
-        help="path to model",
+        help="Path to the model or HuggingFace link.",
     )
     parser.add_argument(
         "--tokenizer",
         type=str,
         default=None,
-        help="path to non-matching tokenizer, requires --direct_load",
+        help="Path to non-matching tokenizer, requires --direct_load.",
     )
     parser.add_argument(
         "--chat_template",
         type=str,
         default="tulu",
-        help="path to chat template",
+        help="Path to the chat template.",
     )
     parser.add_argument(
         "--batch_size",
         type=int,
         default=64,
-        help="batch size for inference (if above number of tokens)",
+        help="Batch size for inference (if above number of tokens).",
     )
     parser.add_argument(
         "--text",
         type=str,
         default="I love to drink coffee at work.",
-        help="text to evaluate",
+        help="Text to evaluate.",
     )
     args = parser.parse_args()
 
-    if "PairRM" in args.model or "PairRM" in args.chat_template or "SHP" in args.model or "SHP" in args.chat_template:
-        # Note: SHP could be used in single-output mode, but the code is not yet added
-        raise ValueError("PairRM and SHP require pairwise inputs, not supported")
+    # Input validation
+    def _validate_require_pairwise_inputs(models):
+        for model in models:
+            if args.model == model or args.chat_template == model:
+                raise ValueError(f"{model} require pairwise inputs, not supported")
+
+    _validate_require_pairwise_inputs(models=["PairRM", "SHP"])
+
     return args
 
 
 def main():
     args = get_args()
-    quantized = True  # only Starling isn't quantized for now
-    custom_dialogue = False
-    # some models need custom code to be run
-    if "oasst" in args.model or "oasst" in args.chat_template:
-        from herm.models import openassistant  # noqa
+    model_name = args.model if args.model in REWARD_MODEL_CONFIG.keys() else "default"
+    config = REWARD_MODEL_CONFIG.get(model_name)
 
-        model_builder = AutoModelForSequenceClassification.from_pretrained
-        pipeline_builder = pipeline
-    elif "Starling" in args.model or "Starling" in args.chat_template:
-        from herm.models.starling import StarlingPipeline, build_starling_rm
-
-        model_builder = build_starling_rm
-        pipeline_builder = StarlingPipeline
-        quantized = False
-    elif "openbmb" in args.model or "openbmb" in args.chat_template:
-        from herm.models.openbmb import LlamaRewardModel, OpenBMBPipeline
-
-        model_builder = LlamaRewardModel.from_pretrained
-        pipeline_builder = OpenBMBPipeline
-    elif "PairRM" in args.model or "PairRM" in args.chat_template:
-        from herm.models.pairrm import DebertaV2PairRM, PairRMPipeline
-
-        custom_dialogue = True
-        model_builder = DebertaV2PairRM.from_pretrained
-        pipeline_builder = PairRMPipeline
-    elif "SHP" in args.model or "SHP" in args.chat_template:
-        from herm.models.shp import SHPPipeline
-
-        custom_dialogue = True
-        model_builder = T5ForConditionalGeneration.from_pretrained
-        pipeline_builder = SHPPipeline
-    else:
-        model_builder = AutoModelForSequenceClassification.from_pretrained
-        pipeline_builder = pipeline
-
-    if custom_dialogue:
+    if config["custom_dialogue"]:
         raise ValueError("Custom dialogue formatting not yet supported in this script")
 
-    ###############
-    # Setup logging
-    ###############
-    accelerator = Accelerator()
+    # Setup the accelerate state first before using logging since it errors out
+    # if you do the other first.
+    accelerator = Accelerator(cpu=True)
     current_device = accelerator.process_index
 
-    logger = get_logger(__name__)
+    # Setup logging
+    logger = setup_logging(name=__name__)
+    logger.info(f"Running reward model on {args.model} with chat template {args.chat_template}")
+
+    # Prepare dataset and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer or args.model)
+
+    def _tokenify_string(string):
+        tokens = tokenizer.tokenize(string)
+        cumulative_texts = [tokenizer.convert_tokens_to_string(tokens[: i + 1]) for i, _ in enumerate(tokens)]
+        return cumulative_texts
+
+    substrings = _tokenify_string(args.text)
+    dataset = Dataset.from_list([{"text": substring} for substring in substrings])
+
+    # Load reward model pipeline
+    logger.info("Loading reward model")
+    reward_pipeline = load_reward_pipeline(
+        args.model,
+        config=config,
+        tokenizer=tokenizer,
+        process_index=current_device,
+    )
+    reward_pipeline_kwargs = {
+        "batch_size": args.batch_size,  # eval_args.inference_batch_size,
+        "truncation": True,
+        "padding": True,
+        "max_length": 2048,
+        "function_to_apply": "none",  # Compute raw logits
+        "return_token_type_ids": False,
+    }
+
+    # Perform inference and get per-token reward
+    per_token_rewards = get_per_token_reward(
+        dataset,
+        reward_pipeline=reward_pipeline,
+        reward_pipeline_kwargs=reward_pipeline_kwargs,
+        accelerator=accelerator,
+        is_custom_pipeline=config["pipeline_builder"] == pipeline,
+        logger=logger,
+        dataloader_batch_size=args.batch_size,
+    )
+
+    # Report the results
+    for reward, token in zip(per_token_rewards, substrings):
+        print(f"Reward: {round(reward, 3)} | Substring: {token}")
+
+
+def setup_logging(name: Optional[str] = None) -> logging.Logger:
+    logger = get_logger(name or __name__)
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
@@ -132,126 +195,84 @@ def main():
     transformers.utils.logging.set_verbosity(log_level)
     transformers.utils.logging.enable_default_handler()
     transformers.utils.logging.enable_explicit_format()
+    return logger
 
-    logger.info(f"Running reward model on {args.model} with chat template {args.chat_template}")
 
-    ############################
-    # Load reward model pipeline
-    ############################
-    tokenizer_path = args.tokenizer if args.tokenizer else args.model
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-
-    BATCH_SIZE = args.batch_size
-    logger.info("*** Load reward model ***")
-    reward_pipeline_kwargs = {
-        "batch_size": BATCH_SIZE,  # eval_args.inference_batch_size,
-        "truncation": True,
-        "padding": True,
-        "max_length": 2048,
-        "function_to_apply": "none",  # Compute raw logits
-        "return_token_type_ids": False,
-    }
-    if quantized:
-        model_kwargs = {
-            "load_in_8bit": True,
-            "device_map": {"": current_device},
-            "torch_dtype": torch.float16 if torch.cuda.is_available() else None,
-        }
-    else:
-        model_kwargs = {"device_map": {"": current_device}}
-    # TODO remove direct load logic
-    # if pipeline_builder is pipeline, use built in pipeline, else custom
+def load_reward_pipeline(
+    model_name: str,
+    *,
+    config: Dict[str, Any],
+    tokenizer: "transformers.PreTrainedTokenizer",
+    process_index: int,
+):
+    model_kwargs = {"device_map": {"": process_index}}
+    if config["quantized"]:
+        model_kwargs.update(
+            {
+                "load_in_8bit": True,
+                "torch_dtype": torch.float16 if torch.cuda.is_available() else None,
+            }
+        )
+    model_builder = config["model_builder"]
+    pipeline_builder = config["pipeline_builder"]
     if not pipeline == pipeline_builder:
-        model = model_builder(args.model, **model_kwargs)
-        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        reward_pipe = pipeline_builder(
+        reward_pipeline = pipeline_builder(
             "text-classification",
-            model=model,
+            model=model_builder(model_name, **model_kwargs),
             tokenizer=tokenizer,
         )
     else:
-        reward_pipe = pipeline(
+        reward_pipeline = pipeline(
             "text-classification",
-            model=args.model,
+            model=model_name,
             tokenizer=tokenizer,
             revision="main",
             model_kwargs=model_kwargs,
         )
+    # Tokenization settings
+    if reward_pipeline.tokenizer.pad_token_id is None:
+        reward_pipeline.model.config.pad_token_id = reward_pipeline.tokenizer.eos_token_id
+        reward_pipeline.tokenizer.pad_token_id = reward_pipeline.tokenizer.eos_token_id
 
-    ############################
-    # Tokenization settings & dataset preparation
-    ############################
-    # set pad token to eos token if not set
-    if reward_pipe.tokenizer.pad_token_id is None:
-        reward_pipe.model.config.pad_token_id = reward_pipe.tokenizer.eos_token_id
-        reward_pipe.tokenizer.pad_token_id = reward_pipe.tokenizer.eos_token_id
+    return reward_pipeline
 
-    def tokenify_string(string, tokenizer):
-        # Tokenize the entire text
-        tokens = tokenizer.tokenize(string)
 
-        cumulative_texts = []
-        # Iterate over each token
-        for i, _ in enumerate(tokens):
-            # Append the current cumulative text to the list
-            cumulative_texts.append(tokenizer.convert_tokens_to_string(tokens[: i + 1]))
-
-        return cumulative_texts
-
-    substrings = tokenify_string(args.text, tokenizer)
-    # create dataset from list of strings substrings with huggingface
-    dataset = [{"text": substring} for substring in substrings]
-    dataset = Dataset.from_list(dataset)
-
-    ############################
-    # Run inference [1/2]" built in transformers
-    ############################
-    # if using HF pipeline, can pass entire dataset and get results
-    # first, handle custom pipelines that we must batch normally
-    if not pipeline_builder == pipeline:
-        logger.info("*** Running forward pass via built in pipeline abstraction ***")
-        # this setup can be optimized slightly with one pipeline call
-        # prepare for inference
-        reward_pipe = accelerator.prepare(reward_pipe)
-
-        rewards = reward_pipe(dataset["text"], **reward_pipeline_kwargs)
-
-    ############################
-    # Run inference [2/2] custom pipelines
-    ############################
-    else:
-        logger.info("*** Running dataloader to collect results ***")
-
+def get_per_token_reward(
+    dataset: Dataset,
+    *,
+    reward_pipeline: "transformers.Pipeline",
+    reward_pipeline_kwargs: Dict[str, Any],
+    accelerator: "Accelerator",
+    is_custom_pipeline: bool,
+    logger: "logging.Logger",
+    dataloader_batch_size: int,
+) -> List[float]:
+    if is_custom_pipeline:
+        logger.info("Running dataloader to collect results")
         dataloader = torch.utils.data.DataLoader(
             dataset,
-            batch_size=BATCH_SIZE,
+            batch_size=dataloader_batch_size,
             collate_fn=None,
             shuffle=False,
             drop_last=False,
         )
-
-        dataloader, model = accelerator.prepare(dataloader, reward_pipe.model)
-        reward_pipe.model = model
+        dataloader, model = accelerator.prepare(dataloader, reward_pipeline.model)
+        reward_pipeline.model = model
 
         results = []
         for step, batch in enumerate(tqdm(dataloader, desc="RM batch steps")):
             logger.info(f"RM inference step {step}/{len(dataloader)}")
-            rewards = reward_pipe(batch["text"], **reward_pipeline_kwargs)
-
-            # for each item in batch, record 1 if chosen > rejected
-            # extra score from dict within batched results (e.g. logits)
-            # [{'label': 'LABEL_1', 'score': 0.6826171875},... ]
-            if isinstance(rewards[0], dict):
-                scores = [result["score"] for result in rewards]
-            # for classes that directly output scores (custom code)
-            else:
-                scores = rewards.cpu().numpy().tolist()
-
+            rewards = reward_pipeline(batch["text"], **reward_pipeline_kwargs)
+            # Some pipeline implementations return a list of dictionaries, if that's the
+            # case, we only take the value in the 'score' key. Else, we just return the list.
+            scores = [r["score"] for r in rewards] if isinstance(rewards[0], dict) else rewards.cpu().numpy().tolist()
             results.extend(scores)
+    else:
+        logger.info("Running forward pass via built-in pipeline abstraction")
+        reward_pipeline = accelerator.prepare(reward_pipeline)
+        results = reward_pipeline(dataset["text"], reward_pipeline_kwargs)
 
-    # print the results
-    for i, substring in enumerate(substrings):
-        print(f"Reward: {round(results[i], 3)} | Substring: {substring}")
+    return results
 
 
 if __name__ == "__main__":
