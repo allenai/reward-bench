92dcdbc82436943e60e1ab8b654b01771382a657
Valentina Pyatkin <valentinapy@ip-172-16-20-20.us-west-2.compute.internal>
2024-02-13 16:14:20 -0800

    test

--- herm/dpo.py
+++ herm/dpo.py
@@ -212,7 +212,8 @@ class DPOInference:
             ) = self.concatenated_forward(self.ref_model, batch)
             chosen_logratios = policy_chosen_logps - ref_chosen_logps
             rejected_logratios = policy_rejected_logps - ref_rejected_logps
-
+            print(chosen_logratios)
+            sys.exit(0)
         else:
             chosen_logratios = policy_chosen_logps
             rejected_logratios = policy_rejected_logps
