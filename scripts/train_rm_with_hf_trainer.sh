6dce77cbc56c71aa2dd44a1d334be3e93bb11c42
jacob-morrison <jacobm@allenai.org>
2024-02-21 15:50:39 -0800

    update

--- scripts/train_rm_with_hf_trainer.sh
+++ scripts/train_rm_with_hf_trainer.sh
@@ -16,45 +16,45 @@ GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
 # OUTPUT_DIR=/net/nfs.cirrascale/allennlp/jacobm/herm/rms/nectar/tulu-2-7b-nectar-full-3_8m-1e-5-linear
 MODEL_PATH=meta-llama/Llama-2-7b-chat-hf
 # TRAIN_DATASET=ultrafeedback # 60k
-# OUTPUT_DIR=net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-uf-60k
-TRAIN_DATASET=nectar-binarized # 180k
-OUTPUT_DIR=net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-180k
-# TRAIN_DATASET=nectar # 700k
-# OUTPUT_DIR=net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-700k
-# TRAIN_DATASET=nectar-full # 3.8m
-# OUTPUT_DIR=net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-3.8m
+# OUTPUT_DIR=/net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-uf-60k
+# TRAIN_DATASET=nectar-binarized # 180k
+# OUTPUT_DIR=/net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-180k
+# # TRAIN_DATASET=nectar # 700k
+# # OUTPUT_DIR=/net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-700k
+# # TRAIN_DATASET=nectar-full # 3.8m
+# # OUTPUT_DIR=/net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-3.8m
 
-# EVAL_DATASET=alpaca_farm_human_preferences
-# TRAIN_DATASET=Anthropic/hh-rlhf
-# TRAIN_DATASET=Dahoas/synthetic-instruct-gptj-pairwise
-# OUTPUT_DIR=models/llama-2-7b-gptj-pairwise-5-epochs
-# OUTPUT_DIR=net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/${DATASET}_${MODEL_SIZE}/
-echo "Training model ${MODEL_PATH} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+# # EVAL_DATASET=alpaca_farm_human_preferences
+# # TRAIN_DATASET=Anthropic/hh-rlhf
+# # TRAIN_DATASET=Dahoas/synthetic-instruct-gptj-pairwise
+# # OUTPUT_DIR=models/llama-2-7b-gptj-pairwise-5-epochs
+# # OUTPUT_DIR=/net/nfs.cirrascale/allennlp/jacobm/modular_adaptation/checkpoints/${DATASET}_${MODEL_SIZE}/
+# echo "Training model ${MODEL_PATH} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
 
-deepspeed --include localhost:0,1,2,3,4,5,6,7 scripts/train_rm_trainer.py \
-    --deepspeed ds_configs/stage3_no_offloading.conf \
-    --model_name_or_path $MODEL_PATH \
-    --tokenizer_name $MODEL_PATH \
-    --dataset_name $TRAIN_DATASET \
-    --max_seq_length 1024 \
-    --preprocessing_num_workers 64 \
-    --do_train \
-    --use_flash_attn \
-    --bf16 \
-    --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
-    --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
-    --learning_rate 1e-5 \
-    --lr_scheduler_type linear \
-    --warmup_ratio 0.03 \
-    --weight_decay 0. \
-    --evaluation_strategy no \
-    --logging_steps 1 \
-    --save_strategy epoch \
-    --seed 12349876 \
-    --num_train_epochs 1 \
-    --output_dir $OUTPUT_DIR \
-    --use_slow_tokenizer \
-    --overwrite_output_dir 
+# deepspeed --include localhost:0,1,2,3,4,5,6,7 scripts/train_rm_trainer.py \
+#     --deepspeed ds_configs/stage3_no_offloading.conf \
+#     --model_name_or_path $MODEL_PATH \
+#     --tokenizer_name $MODEL_PATH \
+#     --dataset_name $TRAIN_DATASET \
+#     --max_seq_length 1024 \
+#     --preprocessing_num_workers 64 \
+#     --do_train \
+#     --use_flash_attn \
+#     --bf16 \
+#     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+#     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+#     --learning_rate 1e-5 \
+#     --lr_scheduler_type linear \
+#     --warmup_ratio 0.03 \
+#     --weight_decay 0. \
+#     --evaluation_strategy no \
+#     --logging_steps 1 \
+#     --save_strategy epoch \
+#     --seed 12349876 \
+#     --num_train_epochs 1 \
+#     --output_dir $OUTPUT_DIR \
+#     --use_slow_tokenizer \
+#     --overwrite_output_dir 
 
     # --save_strategy steps \
     # --save_steps 0.1 \
@@ -77,7 +77,7 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 scripts/train_rm_trainer.py \
     # --max_steps 10 
 
 TRAIN_DATASET=nectar # 700k
-OUTPUT_DIR=net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-700k
+OUTPUT_DIR=/net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-700k
 
 echo "Training model ${MODEL_PATH} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
 
@@ -107,7 +107,7 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 scripts/train_rm_trainer.py \
     --overwrite_output_dir 
 
 TRAIN_DATASET=nectar-full # 3.8m
-OUTPUT_DIR=net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-3.8m
+OUTPUT_DIR=/net/nfs.cirrascale/allennlp/jacobm/herm/rms/ultrafeedback/llama-2-7b-chat-nectar-3.8m
 
 echo "Training model ${MODEL_PATH} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
 
