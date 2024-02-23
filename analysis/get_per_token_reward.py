8cf135b70c88f873fc4d55eda08a6a05889b2914
ljvmiranda921 <ljvmiranda@gmail.com>
2024-02-23 13:06:30 -0800

    Fixup

--- analysis/get_per_token_reward.py
+++ analysis/get_per_token_reward.py
@@ -96,7 +96,7 @@ def get_args():
         default=None,
         help="Random seed for reproducibility.",
     )
-    args = parser.parse_args("")
+    args = parser.parse_args()
 
     # Input validation
     def _validate_require_pairwise_inputs(models):
