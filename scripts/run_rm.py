d973dab1c0e7a2dc6f2971618438b84ea25e03b3
hanklin <55573473+hank0316@users.noreply.github.com>
2024-04-26 02:57:25 +0800

    Add `pad_token_id` from tokenizer to model config. (#117)

--- scripts/run_rm.py
+++ scripts/run_rm.py
@@ -183,6 +183,9 @@ def main():
     if reward_pipe.tokenizer.pad_token_id is None:
         reward_pipe.model.config.pad_token_id = reward_pipe.tokenizer.eos_token_id
         reward_pipe.tokenizer.pad_token_id = reward_pipe.tokenizer.eos_token_id
+    # For models whose config did not contains `pad_token_id`
+    if reward_pipe.model.config.pad_token_id is None:
+        reward_pipe.model.config.pad_token_id = reward_pipe.tokenizer.pad_token_id
 
     # if using fastchat template (no template in tokenizer), make the RM tokenizer output an EOS token
     if not check_tokenizer_chat_template(tokenizer):
