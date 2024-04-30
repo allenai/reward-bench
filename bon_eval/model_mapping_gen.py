e9d88faf3eeb0cb189a80b6c53c90b267f51fc02
yuchenlin <yuchen.lin@usc.edu>
2024-04-30 15:14:57 +0000

    ignoring the special folder that saves the eval results

--- bon_eval/model_mapping_gen.py
+++ bon_eval/model_mapping_gen.py
@@ -5,6 +5,8 @@ file_paths = {}
 for root, dirs, files in os.walk("bon_data/rm_bon_eval_results/best-of-n/alpaca_eval/tulu-13b/"):
     for file in files:
         filepath = str(os.path.join(root, file))
+        if "/eval_results/" in filepath:
+            continue
         rm_name = filepath.replace("bon_data/rm_bon_eval_results/best-of-n/alpaca_eval/tulu-13b/", "").replace(".json", "" )
         url = f"https://huggingface.co/datasets/allenai/reward-bench-results/raw/main/best-of-n/alpaca_eval/tulu-13b/{rm_name}.json"
         if rm_name == "bon_eval_results":
