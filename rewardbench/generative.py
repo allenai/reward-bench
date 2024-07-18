5a5333be63f00385818e4808dcd7c0e695928761
Nathan Lambert <nathanl@allenai.org>
2024-07-18 19:33:01 +0000

    small pr

--- rewardbench/generative.py
+++ rewardbench/generative.py
@@ -56,6 +56,7 @@ OPENAI_MODEL_LIST = (
     "gpt-4-0125-preview",
     "gpt-4-turbo-2024-04-09",
     "gpt-4o-2024-05-13",
+    "gpt-4o-mini-2024-07-18",
 )
 
 # feel free to add more models to this list via PR
