6e81b693278d5bb97b2b628688bbcfeb41677826
kirigayahitsugi <zhangge19951114@163.com>
2024-10-16 17:48:21 +0800

    add general preference model eval

--- scripts/run_rm_rewardbench.sh
+++ scripts/run_rm_rewardbench.sh
@@ -1,16 +1,15 @@
 export CUDA_VISIBLE_DEVICES=0
-
 python run_rm.py \
---model general-preference/GPM-Llama-3.1-8B \
+--model general-preference/GPM-Gemma-2B \
 --chat_template raw \
 --batch_size 64 \
---value_head_dim 2 \
+--value_head_dim 8 \
 --max_length 4096 \
 --do_not_save \
 --trust_remote_code \
 --attn_implementation "flash_attention_2" \
 --is_general_preference \
---add_prompt_head \
+--add_prompt_head 
 
 
 # general-preference/GPM-Llama-3.1-8B
